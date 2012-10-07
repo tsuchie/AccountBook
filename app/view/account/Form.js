6160b19307b19e91e03cc20a39c9c6f810a8f8b8
tsuchie <tsuchie.takuro@gmail.com>
2012-10-07 16:21:08 +0900

    入力画面に保存ボタンを追加する

--- app/view/account/Form.js
+++ app/view/account/Form.js
@@ -11,31 +11,51 @@ Ext.define('Ab.view.account.Form', {
     config: {
         items: [
             {
-                xtype: 'numberfield',
-                name: 'account',
-                label: '金額'
-            },
-            {
-                xtype: 'selectfield',
-                name: 'category',
-                label: 'カテゴリ',
-                options: [
-                    {text: '食費', value: '10'},
-                    {text: '交通', value: '20'},
-                    {text: '交際費', value: '30'},
-                    {text: 'エンタメ', value: '40'},
-                    {text: '水道・光熱', value: '50'}
+                xtype: 'fieldset',
+                items: [
+                    {
+                        xtype: 'numberfield',
+                        name: 'account',
+                        label: '金額'
+                    },
+                    {
+                        xtype: 'selectfield',
+                        name: 'category',
+                        label: 'カテゴリ',
+                        options: [
+                            {text: '食費', value: '10'},
+                            {text: '交通', value: '20'},
+                            {text: '交際費', value: '30'},
+                            {text: 'エンタメ', value: '40'},
+                            {text: '水道・光熱', value: '50'}
+                        ]
+                    },
+                    {
+                        xtype: 'datepickerfield',
+                        name: 'recorded',
+                        label: '日付'
+                    },
+                    {
+                        xtype: 'textareafield',
+                        name: 'memo',
+                        label: 'メモ'
+                    }
                 ]
             },
             {
-                xtype: 'datepickerfield',
-                name: 'recorded',
-                label: '日付'
-            },
-            {
-                xtype: 'textareafield',
-                name: 'memo',
-                label: 'メモ'
+                layout: {
+                    type: 'vbox',
+                    pack:'center',
+                    align: 'center'
+                },
+                items: {
+                    xtype: 'button',
+                    ui: 'action',
+                    text: '記録する',
+                    itemId: 'saveDetailButton',
+                    width: '80%',
+                    minWidth: '200px'
+                }
             }
         ]
     }
