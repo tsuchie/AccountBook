4b9a4d13722f861536d991f464ffdc338d9e506e
tsuchie <tsuchie.takuro@gmail.com>
2012-11-01 11:07:37 +0900

    Dropbox プロキシを更新する

--- ux/data/proxy/Dropbox.js
+++ ux/data/proxy/Dropbox.js
@@ -7,7 +7,9 @@ Ext.define('Ext.ux.data.proxy.Dropbox', {
     config: {
         key: '',
         id: '',
-        rememberUser: false
+        rememberUser: false,
+        enablePagingParams: false,
+        filePath: 'sample.dat'
     },
 
     constructor: function (config) {
@@ -31,15 +33,77 @@ Ext.define('Ext.ux.data.proxy.Dropbox', {
     },
 
     read: function (operation, callback, scope) {
-        console.log('read', arguments);
+        console.log('read');
+        var me = this,
+            path = me.selectFilePath();
+
+        me.cache_ = me.cache_ || {};
+        me.cache_[path] = me.cache_[path] || {};
+
+        me.readFile(path, onDataRead);
+
+        function onDataRead(success, data) {
+            var reader = me.getReader(),
+                Model = me.getModel(),
+                sorters = operation.getSorters(),
+                filters = operation.getFilters(),
+                start = operation.getStart(),
+                limit = operation.getLimit(),
+                resultSet, records, collection;
+
+            try {
+                resultSet = reader.read(data);
+            } catch (e) {
+                operation.setException(e.message);
+                me.fireEvent('exception', me, data, operation);
+                return;
+            }
+
+            records = resultSet.getRecords();
+            me.updateCache(path, records);
+
+            collection = Ext.create('Ext.util.Collection');
+
+            // First we comply to filters
+            if (filters && filters.length) {
+                collection.setFilters(filters);
+            }
+            // Then we comply to sorters
+            if (sorters && sorters.length) {
+                collection.setSorters(sorters);
+            }
+
+            collection.addAll(records);
+
+            if (me.getEnablePagingParams() && start !== undefined && limit !== undefined) {
+                records = collection.items.slice(start, start + limit);
+            } else {
+                records = collection.items.slice();
+            }
+
+            operation.setSuccessful();
+            operation.setCompleted();
+
+            resultSet.setRecords(records);
+            operation.setRecords(records);
+
+            if (typeof callback === 'function') {
+                callback.call(scope || me, operation);
+            }
+        }
     },
 
     update: function (operation, callback, scope) {
-        console.log('update', arguments);
-        var data = operation.getRecords().map(function (rec) {
-            return rec.raw;
-        });
-        this.write('sampledata.json', JSON.stringify(data));
+        console.log('update');
+        var me = this,
+            path = me.selectFilePath(me.cache_),
+            records = operation.getRecords();
+
+        me.updateCache(path, records);
+        this.writeFile(path, JSON.stringify(me.getCacheAsData(path)));
+        if (typeof callback === 'function') {
+            callback.call(scope || this, operation);
+        }
     },
 
     destroy: function (operation, callback, scope) {
@@ -63,11 +127,24 @@ Ext.define('Ext.ux.data.proxy.Dropbox', {
         });
     },
 
-    write: function (filename, data) {
+    readFile: function (path, callback) {
         var me = this,
             client = me.client_;
 
-        client.writeFile(filename, data, function (error, stat) {
+        client.readFile(path, function (error, data) {
+            console.log(path, error, data);
+            if (error) {
+                return me.showError(error);
+            }
+            callback.call(me, true, data);
+        });
+    },
+
+    writeFile: function (path, data) {
+        var me = this,
+            client = me.client_;
+
+        client.writeFile(path, data, function (error, stat) {
             if (error) {
                 return me.showError(error);
             }
@@ -78,6 +155,44 @@ Ext.define('Ext.ux.data.proxy.Dropbox', {
     showError: function (error) {
         //TODO implements
         console.log(error.status);
+    },
+
+    selectFilePath: function (data) {
+        var me = this,
+            path = me.getFilePath();
+
+        if (typeof path === 'function') {
+            path = path.call(me, data);
+        }
+        if (!path || path.length === 0) {
+            //TODO Throw exception?
+            path = 'sample.dat';
+        }
+        return path;
+    },
+
+    updateCache: function (key, records) {
+        var me = this,
+            cache = me.getCache(key);
+
+        records.forEach(function (rec) {
+            cache[rec.getId()] = rec.getData();
+        });
+    },
+
+    getCache: function (key) {
+        var me = this;
+        me.cache_ = me.cache_ || {};
+        me.cache_[key] = me.cache_[key] || {};
+        return me.cache_[key];
+    },
+
+    getCacheAsData: function (key) {
+        var me = this,
+            cache = me.getCache(key);
+        return Object.keys(cache).map(function (id) {
+            return cache[id];
+        });
     }
 
 });
